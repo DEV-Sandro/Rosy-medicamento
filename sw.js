b6ef608af6612889a73a1bb3296628acd1881dd8
Sandro Sousa dos Anjos <103465733+DEV-Sandro@users.noreply.github.com>
2026-09-06 17:51:19 -0300

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,17 @@
+const CACHE = 'jeferson-med-v1';
+const APP = ['./', './index.html', './manifest.webmanifest'];
+
+self.addEventListener('install', event => {
+  event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(APP)).then(() => self.skipWaiting()));
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(self.clients.claim());
+});
+
+self.addEventListener('fetch', event => {
+  if (event.request.method !== 'GET') return;
+  event.respondWith(
+    fetch(event.request).catch(() => caches.match(event.request))
+  );
+});
